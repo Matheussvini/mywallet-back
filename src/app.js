e213df3f6e855bae1ee02bd40d8c1702b12931ef
matheussvini <matheussvini@outlook.com>
2022-11-21 18:00:05 -0300

    configs to deploy database

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use(participantsRouters);
 app.use(transactionsRouters);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
